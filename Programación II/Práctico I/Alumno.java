f24bfb1a43592788202a091ab8fe98dfcd497216
listcodename <angelzero16@hotmail.com>
2024-12-21 10:40:39 -0300

    Primer commit

---
+++ Programación II/Práctico I/Alumno.java
@@ -0,0 +1,18 @@
+public class Alumno{
+    
+}
+
+/**
+ * La clase alumno cuenta con los siguientes atributos:
+ * nombre
+ * apellido
+ * curso (La comisión es Única por lo que se diferencian por el numero del año)
+ * edad
+ * 
+ * Los siguientes métodos:
+ * toString(). Devuelve los datos del profesor en el siguiente formato.
+ * "Roberto Gonzalez - 15 Años"
+ * 
+ * setCurso(). Recibe el curso y verifica que sea válido (de 1 al 7)
+ * 
+ */
---
+++ Programación II/Práctico I/Profesor.java
@@ -0,0 +1,20 @@
+public class Profesor{
+    
+}
+
+/**
+ * La clase profesor cuenta con los siguientes atributos:
+ * nombre
+ * apellido
+ * curso (La comisión es Única por lo que se diferencian por el numero del año)
+ * materia
+ * 
+ * Los siguientes métodos:
+ * toString(). Devuelve los datos del profesor en el siguiente formato.
+ * "Profesor: Roberto Gonzalez"
+ * "Curso: 5to"
+ * "Materia: Ciencias Sociales"
+ * 
+ * setCurso(). Recibe el curso y verifica que sea válido (de 1 al 7)
+ * 
+ */
